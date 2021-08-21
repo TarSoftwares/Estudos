57abbd2c5cc8a4edddf4dfe2f20b6724afc35756
TarSoftwares <tarsoftwares@gmail.com>
2021-08-21 15:07:09 -0300

    load save

--- Front/Angular/Todo/src/app/app.component.ts
+++ Front/Angular/Todo/src/app/app.component.ts
@@ -21,13 +21,15 @@ export class AppComponent {
     // this.Todos.push(new Todo(1,false,'estudar'));
     // this.Todos.push(new Todo(2,false,'dormir'));
     // this.Todos.push(new Todo(3,true,'finalizar aula anterior'));     
+    this.load();
   }
   add(){
     const title = this.form.controls['title'].value;
     const id    = this.Todos.length + 1 ;
     this.Todos.push(new Todo(id,false,title));
+    this.save();
     this.clear();
-  }
+  } 
   clear(){
     this.form.reset();
   }
@@ -38,6 +40,19 @@ export class AppComponent {
       this.title = "Lista de Tarefas"
     else
       this.title = "Minhas Tarefas";  
+  } 
+  save(){
+    const data = JSON.stringify(this.Todos);
+    //'todos' nome da chave armazenada
+    localStorage.setItem('todos',data);
+  }
+  load()
+  {
+    //minha Lista 'todos' sendo carregada
+     const data = localStorage.getItem('todos');    
+     //Erro - atualizando Todos com o JSON recuperado de localStorage, já com o parse.
+     //Aplicação quebra aqui 
+     this.Todos = JSON.parse(localStorage['todos']);     
   }
   remove(todo : Todo)
   {
@@ -45,15 +60,18 @@ export class AppComponent {
      if (item !== -1)
      {
        this.Todos.splice(item,1);
+       this.save();
      }
      
   }
   markAsDone(todo : Todo)
   {
     todo.done = true;
+    this.save();
   }
   markAsUndone(todo : Todo)
   {
     todo.done = false;
+    this.save();
   }
 }
