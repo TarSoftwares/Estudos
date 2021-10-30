dc5351995af1eb9ba6e73d9fcfb9aad81d4cbe5a
TarSoftwares <tarsoftwares@gmail.com>
2021-10-30 16:52:42 -0300

    InputOutPutProp

---
+++ Front/Angular/InputOutPutProp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    CursosModule     
+  ],
+  providers: [],
+  bootstrap: [AppComponent] //Component que será instanciado (Principal)
+})
+export class AppModule { }
