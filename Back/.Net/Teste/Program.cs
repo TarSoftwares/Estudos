ae2fe8e6cdbb33fcaadef15002966791b3ed1760
TarSoftwares <tarsoftwares@gmail.com>
2022-02-28 20:01:54 -0300

    Leitura

---
+++ Back/.Net/Teste/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int maiorIdade  = 18;
+            int idadeLimite = 65;
+            Console.WriteLine("Entre com a Idade");
+            idade = int.Parse(Console.ReadLine());
+            
+            if (idade >= maiorIdade && idade <= idadeLimite)
+            {
+                Console.WriteLine($"Você tem {idade} anos de idade. Pode ser convocado para a Guerra! ");
+            }
+            else
+            {
+                Console.WriteLine($"Você tem {idade} de idade. Não Pode ser convocado para a Guerra! ");
+
+            }
+        }
+    }
+}
