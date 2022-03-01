2decfd5bdba5caccef0a54478dd1c44eea83a47e
TarSoftwares <tarsoftwares@gmail.com>
2022-03-01 19:03:06 -0300

    ConsoleIF

--- Back/.Net/Teste/Program.cs
+++ Back/.Net/Teste/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System;
-using System.IO;
 
 namespace Teste
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            int idade = 25;
-            int maiorIdade = 18;      
-            int idadeLimit = 70;     
-          
-            if (idade >= maiorIdade && idade <= idadeLimit)
+        static void Main(string[] args)
+        {            
+            int idadeMaxima = 65;
+            int maiorIdade  = 18;            
+            Console.WriteLine("Entre com o Nome");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Entre com a idade Limite");
+            int idade = int.Parse(Console.ReadLine());
+            if (idade >= maiorIdade)
             {
-                Console.WriteLine($"Você tem {idade} anos de idade. Pode ser convocado para a Guerra! ");
+                Console.WriteLine($"{Nome} é maior de Idade");
+            }
+            if (idade >= maiorIdade && idade <= idadeMaxima)
+            {
+                Console.WriteLine($"{Nome} Pode ser convocado para a guerra!");
             }
             else
             {
-                Console.WriteLine($"Você tem {idade} de idade. Não Pode ser convocado para a Guerra! ");
-
+                Console.WriteLine($"{Nome} Não Pode ser convocado para a guerra!");
             }
+
+            
         }
     }
 }
