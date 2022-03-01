ec1d0ea44b712c8f76a02bc913def4f05fb36332
TarSoftwares <tarsoftwares@gmail.com>
2022-03-01 20:44:59 -0300

    ConsoleSwitch

--- Back/.Net/ConsoleSwitch/Program.cs
+++ Back/.Net/ConsoleSwitch/Program.cs
@@ -4,9 +4,33 @@ namespace ConsoleSwitch
 {
     class Program
     {
-        static void Main(string[] args)
+        private const bool V = false;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Main(args, false);
+        }
+
+        static void Main(string[] args, bool v)
+        {
+           Console.WriteLine("Entre com o Nome");
+           string Nome = Console.ReadLine();
+           Console.WriteLine("Entre com a idade");
+           int idade = int.Parse(Console.ReadLine());
+           Boolean menorIdade = false;
+           switch (idade)
+           {
+               case < 18:  menorIdade = true;break;
+               case >= 18: menorIdade = false;break;
+               default: menorIdade = false;break;
+           }
+           switch (menorIdade)
+           {
+                case true: Console.WriteLine($"{Nome} é menor de Idade");break;
+                case false: Console.WriteLine($"{Nome} é maior de Idade");break;   
+               default: Console.WriteLine($"{Nome}é menor de Idade");break;
+           } 
+
         }
     }
 }
