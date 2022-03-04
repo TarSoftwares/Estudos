a5780f72826a24b99b5b36946077584653937d14
TarSoftwares <tarsoftwares@gmail.com>
2022-03-04 11:11:09 -0300

    ForWhileDoWhile

--- Back/.Net/ForWhileDoWhile/Program.cs
+++ Back/.Net/ForWhileDoWhile/Program.cs
@@ -6,7 +6,34 @@ namespace ForWhileDoWhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Contando com For
+            Contar50();
+            Contar(30);
+            Contar(0,45);
+        }
+        static void Contar50()
+        {
+            for (int i = 0; i <=50; i++)
+            {
+                Console.WriteLine($"Número {i}");
+            }           
+        }
+        static void Contar(int numero)
+        {
+            var i = 0;
+            do{
+                Console.WriteLine($"Número {i}");
+                i++;
+            } while(i <= numero);
+        }
+        static void Contar(int inicio,int fim)
+        {
+            int cont = 0;
+            while (cont <= fim)
+            {
+                Console.WriteLine($"Atual {cont}");
+                cont++;
+            }
         }
     }
 }
