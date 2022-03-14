94acc5d3834a81a53c4524427ed9b1274ff313d7
TarSoftwares <tarsoftwares@gmail.com>
2022-03-14 16:12:50 -0300

    IdGuid

---
+++ Back/.Net/IdGuid/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IdGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Id = Guid.NewGuid();
+            Console.WriteLine(Id.ToString().Substring(0,8));
+
+        }
+    }
+}
