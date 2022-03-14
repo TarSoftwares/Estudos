4ce2673f30c23ff88fc0057ba689ab8b22ad13dd
TarSoftwares <tarsoftwares@gmail.com>
2022-03-14 11:16:07 -0300

    TextEditor

--- Back/.Net/TextEditor/Program.cs
+++ Back/.Net/TextEditor/Program.cs
@@ -6,7 +6,16 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();        
         }
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("          M E N U             ");
+        Console.WriteLine("Digite - 1 Para Criar Arquivo ");
+        Console.WriteLine("Digite - 2 Para Editar Arquivo");
+        Console.WriteLine("Digite - 0 Sair");
+        var Opcao = Int32.Parse(Console.ReadLine());
     }
+    }    
 }
