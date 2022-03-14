0f0ed23f13f50d8e61bf2c63ca1a82d0f39de6e7
TarSoftwares <tarsoftwares@gmail.com>
2022-03-14 17:06:22 -0300

    Strings

--- Back/.Net/Strings/Program.cs
+++ Back/.Net/Strings/Program.cs
@@ -6,7 +6,19 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Teste = "Testando Comparações de Strings";
+            Console.WriteLine(Teste.CompareTo("testando comparações de strings"));
+            var Teste2 = "Testando Comparações de Strings";
+            Console.WriteLine(Teste2.Contains("testando"));
+            Console.WriteLine(Teste2.Contains("testando",StringComparison.OrdinalIgnoreCase));
+            var Teste3 = "Testando Início";
+            Console.WriteLine(Teste3.StartsWith("Testando"));
+            Console.WriteLine(Teste3.StartsWith("testando"));
+            var Teste4 = "Testando Fim";
+            Console.WriteLine(Teste4.EndsWith("Fim"));
+            Console.WriteLine(Teste4.EndsWith("fim"));
+            var Teste5 = "Comparando Texto Exato";
+            Console.WriteLine(Teste5.Equals("Comparando Texto Exato"));
         }
     }
 }
