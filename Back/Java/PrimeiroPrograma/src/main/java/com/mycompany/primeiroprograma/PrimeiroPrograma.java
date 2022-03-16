4f3f7879c6551907eac4d8875e94735000cf8ddc
TarSoftwares <tarsoftwares@gmail.com>
2022-03-16 11:25:59 -0300

    Strings

---
+++ Back/Java/PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author Tarso
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
