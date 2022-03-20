dcf266750053ba3361cfdd4ee876369682052c14
TarSoftwares <tarsoftwares@gmail.com>
2022-03-19 21:49:51 -0300

    MegaSena

---
+++ Back/.Net/MegaSena/Menu.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace MegaSena
+{
+    class Menu{        
+        public static List<Int32> dezenas = new List<Int32>();
+        public static Int32 opcao = new Int32();        
+        public static Int32 Opcao
+        { 
+            get{return opcao;} 
+            set{opcao = value;} 
+        }
+        public static List<Int32> Dezenas
+        { 
+            get{return dezenas;} 
+            set{dezenas = value;} 
+        }
+        public Menu()
+        {           
+            
+        }
+        public static void Exibir ()
+        {                     
+            Console.WriteLine("************M E N U*********");          
+            Console.WriteLine("");
+            Console.WriteLine("1 - Gerar  Dezenas");
+            Console.WriteLine("2 - Exibir Dezenas");
+            Console.WriteLine("3 - Salvar Dezenas");
+            Console.WriteLine("4 - Sair");
+            Int32 v = Int32.Parse(Console.ReadLine());  
+            switch (v)
+            {
+                case 1:
+                {
+                   Menu.GerarDezenas();   
+                   Menu.Exibir();               
+                   break;   
+                }  
+                case 2:
+                {                    
+                    Menu.ExibirDezenas();       
+                    Menu.Exibir();                                                
+                    break;
+                }     
+                case 3:
+                {
+                    Menu.GravarDezenas();
+                    Menu.Exibir();
+                    break;
+                }
+                case 4:
+                {
+                    System.Environment.Exit(0);
+                    break;
+                }             
+                default:
+                {
+                   Menu.Exibir();               
+                   break;
+                }
+            }                              
+        }
+        public static void GerarDezenas()
+        {            
+            List<Int32> lista = new List<Int32>();
+            while (lista.Count < 6)
+            {
+                var dezena = 0;
+                Random random = new Random();
+                dezena = random.Next(1,60);
+                lista.Add(dezena);               
+            }                     
+            Menu.Dezenas = lista;                         
+        }  
+        public static void ExibirDezenas()
+        {            
+            var cont = 0;
+            foreach (Int32 dez in Menu.Dezenas)
+            {
+                cont++;
+                Console.WriteLine($"Dezena {cont} - {dez.ToString()}");
+            }    
+
+        }
+        public static void GravarDezenas()
+        {
+            StreamWriter sw = new StreamWriter("C:\\Tarso\\Jogos\\CSharp.txt");
+            var cont = 0;
+            foreach (var dezenas in Menu.Dezenas)
+            {
+                cont++;
+                sw.WriteLine($"Dezena {cont} -  {dezenas.ToString()}");
+            }
+            sw.Close();
+        }   
+
+
+    }
+}
